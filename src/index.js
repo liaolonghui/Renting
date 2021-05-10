5170141cb3ab8ff7c8efa2828997611ab53cf627
liaolonghui <1340881930@qq.com>
2021-05-10 19:49:08 +0800

    项目初始化

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// antd-mobile的样式
+import 'antd-mobile/dist/antd-mobile.css';
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
