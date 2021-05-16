c1c300d9754a4ccd345d045b04e8d06e71778231
liaolonghui <1340881930@qq.com>
2021-05-16 17:39:28 +0800

    采用可视区渲染的方式渲染列表

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 // antd-mobile的样式
-import 'antd-mobile/dist/antd-mobile.css';
-// 全局样式
-import './index.css';
+import 'antd-mobile/dist/antd-mobile.css'
+// react-virtualized的样式
+import 'react-virtualized/styles.css'
 // 导入字体图标
 import './assets/fonts/iconfont.css'
+// 全局样式
+import './index.css'
 
 
 ReactDOM.render(
--- src/pages/CityList/index.js
+++ src/pages/CityList/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import axios from 'axios'
-import { NavBar, Icon } from 'antd-mobile'
+import { NavBar } from 'antd-mobile'
+import { List, AutoSizer } from 'react-virtualized'
 import { getCurrentCity } from '../../utils/index'
 import './index.scss'
 
@@ -25,6 +26,43 @@ const formatCityList = (list) => {
     cityList,
     cityIndex
   }
+}
+
+// List data as an array of strings
+const list = [
+  'Brian Vaughn',
+  'Brian Vaughn',
+  'Brian Vaughn',
+  'Brian Vaughn',
+  'Brian Vaughn',
+  'Brian Vaughn',
+  'Brian Vaughn',
+  'Brian Vaughn',
+  'Brian Vaughn',
+  'Brian Vaughn',
+  'Brian Vaughn',
+  'Brian Vaughn',
+  'Brian Vaughn',
+  'Brian Vaughn',
+  'Brian Vaughn',
+  'Brian Vaughn',
+  'Brian Vaughn',
+  'Brian Vaughn',
+  // And so on...
+];
+// 渲染列表每一行数据
+function rowRenderer({
+  key, // 唯一key
+  index, // 索引
+  isScrolling, // 当前项是否滚动
+  isVisible, // 当前项是否可见
+  style, // 重点：一定要给每一行数据添加style，用于指定每一行位置
+}) {
+  return (
+    <div key={key} style={style}>
+      {list[index]}{index}{isScrolling+''}{isVisible+''}
+    </div>
+  );
 }
 
 export default class CityList extends React.Component {
@@ -59,6 +97,18 @@ export default class CityList extends React.Component {
           icon={ <i className="iconfont icon-back" /> }
           onLeftClick={() => this.props.history.go(-1)}
         >城市选择</NavBar>
+        {/* List */}
+        <AutoSizer>
+          {({height, width}) => (
+            <List
+              width={width}
+              height={height}
+              rowCount={list.length}
+              rowHeight={50}
+              rowRenderer={rowRenderer}
+            />
+          )}
+        </AutoSizer>
       </div>
     )
   }
