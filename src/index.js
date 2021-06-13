f87baa769574c2046c0d3db8e6117135ccfddc1d
liaolonghui <1340881930@qq.com>
2021-06-13 20:25:49 +0800

    antd-mobile按需加载

--- src/index.js
+++ src/index.js
@@ -2,17 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 
-// antd-mobile的样式
-import 'antd-mobile/dist/antd-mobile.css'
+// antd-mobile的样式 （已开启按需加载，所以不用全部导入了）
+// import 'antd-mobile/dist/antd-mobile.css'
+
 // react-virtualized的样式
 import 'react-virtualized/styles.css'
 // 导入字体图标
 import './assets/fonts/iconfont.css'
-// 全局样式
-import './index.css'
 
 // App组件的导入放后面，防止前面这些样式覆盖组件中的样式
 import App from './App'
+
+// 全局样式 （放后面，防止样式覆盖）
+import './index.css'
 
 ReactDOM.render(
   <App />,
